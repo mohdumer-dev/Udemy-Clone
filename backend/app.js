c1c6ae44342c163afd4000cf5c53cc8976763a07
umerbaba1 <umerbab567@gmail.com>
2025-03-21 23:25:36 +0530

    Added Cloud Files

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,37 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-const  app =express()
+import express from "express";
+import cookieParser from "cookie-parser";
+const app = express();
 
 // Middlewares
-app.use(express.json())
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(cookieParser());
+import fileUpload from "express-fileupload";
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 //  Routes
 
 //user//
-import User from './routes/User.js'
-app.use('/api/v1',User)
+import User from "./routes/User.js";
+app.use("/api/v1", User);
 
 // instrcutor//
-import Instrcutor from './routes/Instrcutor.js'
-app.use('/api/v1',Instrcutor)
-
+import Instrcutor from "./routes/Instrcutor.js";
+app.use("/api/v1", Instrcutor);
 
 // Port
-app.listen(4000,()=>{
-    console.log("Server Working fine")
-})
-
+app.listen(4000, () => {
+  console.log("Server Working fine");
+});
 
 // Database
-import {ConnectDb} from './config/db.js'
-ConnectDb()
+import { ConnectDb } from "./config/db.js";
+ConnectDb();
 
 //  Cloudinary
-import { ConnectToCloudinary } from './config/cloudinary.js'
-ConnectToCloudinary()
+import { ConnectToCloudinary } from "./config/cloudinary.js";
+ConnectToCloudinary();
--- backend/utils/ImageUploader.js
+++ backend/utils/ImageUploader.js
@@ -2,7 +2,7 @@ import { v2 as cloudinary } from 'cloudinary';
 
 export const UploadImage= async (file,folder,quality,height)=>{
 
-    const options={folder}
+    const options={folder, allowed_formats: ["jpg", "png", "gif", "bmp", "webp", "tiff", "svg", "heic"]}
     if(quality){
         options.quality=quality
     }
@@ -10,5 +10,10 @@ export const UploadImage= async (file,folder,quality,height)=>{
         options.height=height
     }
 
+   try{
     return await cloudinary.uploader.upload(file.tempFilePath,options)
+   }catch(err){
+    return res.status(500).json({success:false,msg:"Sever Down while uploading the File to Cloud"})
+   }
+
 }
